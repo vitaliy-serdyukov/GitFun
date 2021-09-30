f8288be15989ce097b9226b5d29ba5bac380e7ba
vital <vitaliy_serdyukov@hotmail.com>
2021-09-30 19:42:08 +0200

    first commit

---
+++ src/GitTest.java
@@ -0,0 +1,8 @@
+public class GitTest {
+
+  public static void main(String[] args) {
+
+    System.out.println("Lets write something cool on the screen");
+    System.out.println("This is one changed line");
+  }
+}
