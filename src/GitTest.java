3b11c92f646a970d40a5499c4b4214ad191ed255
Vitaliy <vitaliy_serdyukov@hotmail.com>
2021-09-30 20:14:11 +0200

    something in feature 1

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,5 +5,10 @@ public class GitTest {
     System.out.println("Lets write something cool on the screen");
     System.out.println("This is one changed line");
     System.out.println("This is two changed line");
+    showFeat1();
+  }
+
+  public static void showFeat1(){
+    System.out.println("This if the method for printing feature 1");
   }
 }
